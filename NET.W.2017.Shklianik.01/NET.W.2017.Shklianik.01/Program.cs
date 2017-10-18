4a5d8dbd6e541511c62787c320bc5ba707a38881
VAL <val.shklianik@gmail.com>
2017-10-19 02:34:47 +0300

    sorts

---
+++ NET.W.2017.Shklianik.01/NET.W.2017.Shklianik.01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NET.W._2017.Shklianik._01.Sorts;
+
+namespace NET.W._2017.Shklianik._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] unsortedArr = { 4, 6, 1, 22, 3, 2, 9, 8, 66, 9, 6, 3 };
+             QuickSort.Sort(unsortedArr);
+            foreach (int element in unsortedArr)
+            {
+                System.Console.Write(element + ",");
+            }
+            Console.WriteLine();
+
+            int[] unsortedArr2 = { 4, 77, 33, 11, 2, 6, 8, 09, 6, 3, 2 };
+            MergeSort.Sort(unsortedArr2);
+            foreach (int element in unsortedArr2)
+            {
+                System.Console.Write(element + ",");
+            }
+           
+
+        }
+    }
+
+
+    
+}
---
+++ NET.W.2017.Shklianik.01/NET.W.2017.Shklianik.01/Sorts/Sorts.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.W._2017.Shklianik._01.Sorts
+{
+    class QuickSort
+    {
+
+        public static void Sort(int[] elements)
+        {
+             Quicksort(elements, 0, elements.Length - 1);
+        }
+
+        private static void Quicksort(int[] elements, int left, int right)
+        {
+            int i = left, j = right;
+            int pivot = elements[(left + right) / 2];
+
+            while (i <= j)
+            {
+                while (elements[i] < pivot)
+                {
+                    i++;
+                }
+
+                while (elements[j] > pivot)
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    // Swap
+                    int tmp = elements[i];
+                    elements[i] = elements[j];
+                    elements[j] = tmp;
+
+                    i++;
+                    j--;
+                }
+            }
+
+            // Recursive calls
+            if (left < j)
+            {
+                Quicksort(elements, left, j);
+            }
+
+            if (i < right)
+            {
+                Quicksort(elements, i, right);
+            }
+
+            
+        }
+
+    }
+
+    class MergeSort
+    {
+        public static void Sort(int[] elements)
+        {
+            Mergesort(elements, 0, elements.Length - 1);
+        }
+
+        private static void Mergesort( int[] elements, int left, int right)
+        {
+            if (left < right)
+            {
+                int middle = (left + right) / 2;
+                Mergesort( elements,left, middle);
+                Mergesort( elements, middle + 1, right);
+                Merge( elements,left, middle, right);
+            }
+        }
+
+        private static void Merge( int[] elements, int left, int middle, int right)
+        {
+            int lengthLeft = middle - left + 1;
+            int lengthRight = right - middle;
+            int[] leftArr = new int[lengthLeft + 1];
+            int[] rightArr = new int[lengthRight + 1];
+            for (int i = 0; i < lengthLeft; i++)
+            {
+                leftArr[i] = elements[left + i];
+            }
+            for (int j = 0; j < lengthRight; j++)
+            {
+                rightArr[j] = elements[middle + j +1];
+            }
+            leftArr[lengthLeft] = Int32.MaxValue;
+            rightArr[lengthRight] = Int32.MaxValue;
+            int iIndex = 0;
+            int jIndex = 0;
+            for (int k = left; k <= right; k++)
+            {
+                if (leftArr[iIndex] <= rightArr[jIndex])
+                {
+                    elements[k] = leftArr[iIndex];
+                    iIndex++;
+                }
+                else
+                {
+                    elements[k] = rightArr[jIndex];
+                    jIndex++;
+                }
+            }
+        }
+    }
+}
