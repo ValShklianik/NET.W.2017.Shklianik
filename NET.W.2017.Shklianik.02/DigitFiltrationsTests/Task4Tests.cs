dd6145f87321e0746f60e68e3e39719ce2b2dad8
VAL <val.shklianik@gmail.com>
2017-10-22 22:56:56 +0300

    task 4 FilterDigit

--- NET.W.2017.Shklianik.02/DigitFiltrationsTests/Task4Tests.cs
+++ NET.W.2017.Shklianik.02/DigitFiltrationsTests/Task4Tests.cs
@@ -12,12 +12,24 @@ namespace DigitFiltrations.Tests
     public class Task4Tests
     {
         [TestMethod()]
-        public void FilterDigitTest_InputListOfNumber1_3_7_55_4_15_56_253_33_Expected55_15_56_253()
+        public void FilterDigitTest_InputListOfNumber1_3_7_55_4_15_56_253_33WithDigit5_Expected55_15_56_253()
         {
             Task4 task4 = new Task4();
             List<int> list = new List<int> { 1, 3, 7, 55, 4, 15, 56, 253, 33 };
             int digit = 5;
             List<int> expected = new List<int> { 55,  15, 56, 253 };
+            List<int> actual = task4.FilterDigit(list, digit);
+            // Assert.AreEqual(actual.Count,expected.Count);
+            Assert.IsTrue(actual.SequenceEqual(expected));
+        }
+
+        [TestMethod()]
+        public void FilterDigitTest_InputListOfNumber1_3_7_55_4_15_56_253_33WithDigit8_ExpectedEmpty()
+        {
+            Task4 task4 = new Task4();
+            List<int> list = new List<int> { 1, 3, 7, 55, 4, 15, 56, 253, 33 };
+            int digit = 8;
+            List<int> expected = new List<int>();
             List<int> actual = task4.FilterDigit(list, digit);
             // Assert.AreEqual(actual.Count,expected.Count);
             Assert.IsTrue(actual.SequenceEqual(expected));
