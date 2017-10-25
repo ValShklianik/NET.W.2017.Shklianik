93a865b5c9c1c3f56444cb5b94d2c8480a33abd6
VAL <val.shklianik@gmail.com>
2017-10-26 02:51:01 +0300

    binConverter and Nod

---
+++ NET.W.2017.Shklianik.03_04/NET.W.2017.Shklianik.03_04/NOD.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.W._2017.Shklianik._03_04
+{
+    public class NOD
+    {
+        #region public methods
+
+        /// <summary>
+        /// The GetTime method which call Euclid algorithm .
+        /// </summary>
+        /// <param name="operationTime">first number</param>
+        /// <param name="firstEl">second number</param>
+        /// <param name="params">second number</param>
+        /// <returns>Greatest common divisor of  numbers</returns>
+
+        public int GetTime(out TimeSpan operationTime, int firsEl, params int[] integers)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            int result = GetNodByEvklid(firsEl,integers);
+
+            watch.Stop();
+            operationTime = watch.Elapsed;
+            return result;
+        }
+        public int GetNodByEvklid(int firstEl, params int[] integers)
+        {
+            firstEl = Math.Abs(firstEl);
+            if (integers.Length == 0)
+            {
+                return firstEl;
+            }
+
+            int secondEl = Math.Abs(integers[0]);
+
+            if (firstEl ==0 || secondEl == 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            while (firstEl != secondEl)
+            {
+                if (firstEl > secondEl)
+                {
+                    firstEl = firstEl - secondEl;
+                }
+                else
+                {
+                    secondEl = secondEl - firstEl;
+                }
+            }
+
+            var others = new int[integers.Length - 1];
+
+            for(int i = 0; i < others.Length; i++)
+            {
+                others[i] = integers[i + 1];
+            }
+
+            return GetNodByEvklid(firstEl, others);
+        }
+
+
+        public int GetNodByStein(int firstEl, params int[] integers)
+        {
+            firstEl = Math.Abs(firstEl);
+
+            if (integers.Length == 0)
+            {
+                return firstEl;
+            }
+            int secondEl = Math.Abs(integers[0]);
+
+            var others = new int[integers.Length - 1];
+      
+            for (int i = 0; i < others.Length; i++)
+            {
+                others[i] = integers[i + 1];
+            }
+
+            return GetNodByStein(GetNod(firstEl, secondEl), others);
+        }
+#endregion 
+
+
+
+        #region private GetNod
+        private int GetNod(int firstEl, int secondEl)
+        {
+            if (firstEl == secondEl)
+                return firstEl;
+            if (firstEl == 0)
+                return secondEl;
+            if (secondEl == 0)
+                return firstEl;
+            if ((~firstEl & 1) != 0)
+            {
+                if ((secondEl & 1) != 0)
+                    return GetNod(firstEl >> 1, secondEl);
+                else
+                    return GetNod(firstEl >> 1, secondEl >> 1) << 1;
+            }
+            if ((~secondEl & 1) != 0)
+                return GetNod(firstEl, secondEl >> 1);
+            if (firstEl > secondEl)
+                return GetNod((firstEl - secondEl) >> 1, secondEl);
+            return GetNod((secondEl - firstEl) >> 1, firstEl);
+
+        }
+#endregion 
+    }
+
+    public static class Converter
+    {
+        /// <summary>
+        /// The NumConverter method which  call GetBin method.
+        /// </summary>
+        /// <param name="doubNumber">first number</param>
+        /// <returns>converted number to binary String</returns>
+        public static string NumConverter(double doubNumber)
+        {
+            string sign = "";
+            if (doubNumber < 0)
+            {
+                sign = "1";
+            }
+            else
+            {
+                sign = "0";
+            }
+            
+            long wholePart = (long)Math.Abs(doubNumber);
+            double fractPart = Math.Abs(doubNumber) - wholePart;
+            string binWholePart =  GetBin(wholePart);
+            string binFractPart = "";
+            int mantisLength = 52;
+            int exp = binWholePart.Length - 1;
+
+            while( (mantisLength- binFractPart.Length-exp) > 0)
+            {
+               
+                fractPart *= 2;
+                if (fractPart >= 1)
+                {
+                    binFractPart += "1";
+                    fractPart -= 1;
+                }
+                else binFractPart += "0";
+            }
+
+            if(binWholePart.Length != 0)
+            {
+                binWholePart = binWholePart.Remove(0, 1);
+            }
+            string mantis = binWholePart+binFractPart;
+            string binExp = GetBin(exp + 1023);
+            string binNumber = sign + binExp + mantis;
+
+            return binNumber;
+        }
+
+#region private GetBin
+        private static string GetBin(long num)
+        {
+            string bin = "";
+            while (num != 0)
+            {
+                if (num % 2 == 1)
+                {
+                    bin = "1" + bin;
+                }
+                else bin = "0" + bin;
+                num /= 2;
+            }
+            while (bin.StartsWith("0"))
+            {
+                bin = bin.Remove(0, 1);
+            }
+
+            return bin;
+        }
+#endregion
+
+    }
+}
---
+++ NET.W.2017.Shklianik.03_04/NET.W.2017.Shklianik.03_04Tests/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+using NET.W._2017.Shklianik._03_04;
+
+namespace NET.W._2017.Shklianik._03_04Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void GetNodByEvklid_params6_9_12_Expected_3()
+        {
+            NOD nodEvklid = new NOD();
+            int actual = nodEvklid.GetNodByEvklid(6,9,12);
+            int expected = 3;
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void GetNodByStein_params6_9_12_Expected_3()
+        {
+            NOD nodEvklid = new NOD();
+            int actual = nodEvklid.GetNodByStein(6, 9, 12);
+            int expected = 3;
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestCase(-255.255, ExpectedResult = "1100000001101111111010000010100011110101110000101000111101011100")]
+        [TestCase(255.255, ExpectedResult = "0100000001101111111010000010100011110101110000101000111101011100")]
+        [TestCase(4294967295.0, ExpectedResult = "0100000111101111111111111111111111111111111000000000000000000000")]
+        //[TestCase(double.MinValue, ExpectedResult = "1111111111101111111111111111111111111111111111111111111111111111")]
+        //[TestCase(double.MaxValue, ExpectedResult = "0111111111101111111111111111111111111111111111111111111111111111")]
+        //[TestCase(double.Epsilon, ExpectedResult = "0000000000000000000000000000000000000000000000000000000000000001")]
+        //[TestCase(double.NaN, ExpectedResult = "1111111111111000000000000000000000000000000000000000000000000000")]
+        //[TestCase(double.NegativeInfinity, ExpectedResult = "1111111111110000000000000000000000000000000000000000000000000000")]
+        //[TestCase(double.PositiveInfinity, ExpectedResult = "0111111111110000000000000000000000000000000000000000000000000000")]
+
+        public string Converter_double_expected(double number)
+        {
+ 
+            return Converter.NumConverter(number);
+        }
+    }
+}
