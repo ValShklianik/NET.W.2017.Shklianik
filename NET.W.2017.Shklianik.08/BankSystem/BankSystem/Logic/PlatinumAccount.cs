ce8fa1657c47c0f7f814dec2d32cf9cefcbcd0ef
VAL <val.shklianik@gmail.com>
2017-11-16 00:27:56 +0300

    add platinum acc

---
+++ NET.W.2017.Shklianik.08/BankSystem/BankSystem/Logic/PlatinumAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem.Account
+{
+    public class PlatinumAccount : Account
+    {
+        public PlatinumAccount(string accountNumber, string name) : base(accountNumber, name)
+        {
+
+        }
+        protected override bool IsValidBalance(decimal value) => value > -5000;
+
+        protected override int CalculateBanefitPoints(decimal amount) => (int)Math.Round(amount * 0.06m + Balance * 0.06m);
+    }
+}
