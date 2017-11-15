972eeeb8f3ef2039c9dfc5750e26d6d8e0fa0de8
VAL <val.shklianik@gmail.com>
2017-11-16 01:18:39 +0300

    new acc service

--- NET.W.2017.Shklianik.08/BankSystem/BankSystem/Logic/Account.cs
+++ NET.W.2017.Shklianik.08/BankSystem/BankSystem/Logic/Account.cs
@@ -105,34 +105,5 @@ namespace BankSystem.Account
     {
         public string Create() => Guid.NewGuid().ToString();
     }
-
-    public class AccountService
-    {
-        private IRepository repository;
-        public AccountService(IRepository repository)
-        {
-            this.repository = repository;
-        }
-
-        public void OpenAccount(string name, AccountType accountType, IAccountNumberCreator creator)
-        {
-            Account account = null;
-            string accountNumber = creator.Create();
-            switch (accountType)
-            {
-                case AccountType.Base:
-                    account = new BaseAccount(accountNumber, name);
-                    break;
-
-                case AccountType.Gold:
-                    account = new GoldAccount(accountNumber, name);
-                    break;
-
-                case AccountType.Platinum:
-                    account = new PlatinumAccount(accountNumber, name);
-                    break;
-            }
-        }
-    }
     
 }
--- NET.W.2017.Shklianik.08/BankSystem/BankSystem/Logic/AccountService.cs
+++ NET.W.2017.Shklianik.08/BankSystem/BankSystem/Logic/AccountService.cs
@@ -4,9 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BankSystem.Logic
+namespace BankSystem.Account
 {
-    class AccountService
+    public class AccountService
     {
+        private IRepository repository;
+        public AccountService(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public void OpenAccount(string name, AccountType accountType, IAccountNumberCreator creator)
+        {
+            Account account = null;
+            string accountNumber = creator.Create();
+            switch (accountType)
+            {
+                case AccountType.Base:
+                    account = new BaseAccount(accountNumber, name);
+                    break;
+
+                case AccountType.Gold:
+                    account = new GoldAccount(accountNumber, name);
+                    break;
+
+                case AccountType.Platinum:
+                    account = new PlatinumAccount(accountNumber, name);
+                    break;
+            }
+        }
     }
 }
