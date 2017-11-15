fc9e72cf13cfbc2337e3a26b2d66543225ce2eba
VAL <val.shklianik@gmail.com>
2017-11-16 01:12:52 +0300

    IRepository changes

--- NET.W.2017.Shklianik.08/BankSystem/BankSystem/Logic/IRepository.cs
+++ NET.W.2017.Shklianik.08/BankSystem/BankSystem/Logic/IRepository.cs
@@ -4,9 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BankSystem.Logic
+namespace BankSystem.Account
 {
-    interface IRepository
+    public interface IRepository 
     {
+        /// <summary>
+        /// Returns all accounts.
+        /// </summary>
+        /// <returns>Books contained in the storage.</returns>
+        IEnumerable<Account> Read();
+
+        /// <summary>
+        /// Saves changes.
+        /// </summary>
+        /// <param name="accounts">Accounts to save</param>
+        void Save(IEnumerable<Account> accounts);
     }
 }
