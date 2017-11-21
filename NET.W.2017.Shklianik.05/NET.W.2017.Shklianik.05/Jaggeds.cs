6967972960553b45f80d8dd4e5ff54211190abfa
VAL <val.shklianik@gmail.com>
2017-11-22 01:08:38 +0300

    task 9-10: Adapter from Comparision to the interface IComparer

--- NET.W.2017.Shklianik.05/NET.W.2017.Shklianik.05/Jaggeds.cs
+++ NET.W.2017.Shklianik.05/NET.W.2017.Shklianik.05/Jaggeds.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NET.W._2017.Shklianik._05
 {
@@ -141,15 +138,48 @@ namespace NET.W._2017.Shklianik._05
             }
         }
 
+        /// <inheritdoc />
+        /// <summary>
+        /// The class that adapts the delegate COmparision to the interface ICOmparer.
+        /// </summary>
+        public class AdapterComparision : IComparer
+        {
+            private Comparison<int[]> comparision;
+            /// <summary>
+            /// Initializes an adapter instance.
+            /// </summary>
+            /// <param name="comparision">the delegate for the conversion to the interface ICOmparer</param>
+            /// <exception cref="ArgumentNullException">Thrown when <paramref name="comparision"/> is null.</exception>
+            public AdapterComparision(Comparison<int[]> comparision)
+            {
+                if (ReferenceEquals(comparision, null))
+                {
+                    throw new ArgumentNullException(nameof(comparision));
+                }
+                this.comparision = comparision;
+            }
+
+            /// <inheritdoc />
+            public int Compare(int[] lhs, int[] rhs)
+            {
+                return comparision(lhs, rhs);
+            }
+        }
+
         public static class ArrayHelper
         {
+
+            public static void Sort(int[][] jaggedArray, Comparison<int[]> comparision) => BubbleSort(jaggedArray, new AdapterComparision(comparision));
+
+            public static void Sort(int[][] jaggedArray, IComparer comparer) => BubbleSort(jaggedArray, comparer);
+
             /// <summary>
             /// The BubbleSort method which sorts of elements.
             /// </summary>
             /// <param name="jaggedArray"> array</param>
             /// <param name="comarer">comparer</param>
             /// <returns>sortind array</returns>
-            public static void BubbleSort(int[][] jaggedArray, IComparer comparer)
+            private static void BubbleSort(int[][] jaggedArray, IComparer comparer)
             {
                 if (jaggedArray == null)
                 {
@@ -168,7 +198,37 @@ namespace NET.W._2017.Shklianik._05
             }
         }
 
-       
+        public static class ArrayHelper2
+        {
+            public static void Sort(int[][] jaggedArray, IComparer comparer) => BubbleSort(jaggedArray, comparer.Compare);
+            public static void Sort(int[][] jaggedArray, Comparison<int[]> comparison) => BubbleSort(jaggedArray, comparison);
+
+            /// <summary>
+            /// The BubbleSort method which sorts of elements.
+            /// </summary>
+            /// <param name="jaggedArray"> array</param>
+            /// <param name="comarer">comparer</param>
+            /// <returns>sortind array</returns>
+            private static void BubbleSort(int[][] jaggedArray, Comparison<int[]> comparer)
+            {
+                if (jaggedArray == null)
+                {
+                    throw new ArgumentNullException(nameof(jaggedArray));
+                }
+                for (int i = 1; i < jaggedArray.Length; i++)
+                {
+                    for (int j = 0; j < jaggedArray.Length - 1; j++)
+                    {
+                        if (comparer(jaggedArray[j], jaggedArray[j + 1]) > 0)
+                        {
+                            Swap(ref jaggedArray[j], ref jaggedArray[j + 1]);
+                        }
+                    }
+                }
+            }
+        }
+
+
         public static void Swap<T>(ref T a, ref T b)
         {
             T t = a;
@@ -179,5 +239,7 @@ namespace NET.W._2017.Shklianik._05
         public static int SumOfRowElemets(int[] array) => array.Sum();
         #endregion
     }
+
+
 
 }
