43257e27ade9600aeca61908da2e605d2a19177d
VAL <val.shklianik@gmail.com>
2017-11-22 04:07:28 +0300

    binary search start impl

---
+++ NET.W.2017.Shklianik.11_12/BinarySearch/BinarySearchTests/TestClass.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using BinarySearchLogic;
+
+namespace BinarySearchTests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        public static int TypeGet(int lhs, int rhs) 
+        {
+            return lhs > rhs ? 1 : -1;
+        }
+
+        [TestCase(new[] { 1, 2, 3, 4, 5, 6, 7 }, 4)]
+        public void Test(int[] array, int req)
+        {
+
+            BinarySearch.Find(array, req, TypeGet);
+            
+        }
+
+
+    }
+}
