7dda2d59edc8a620d5e6f0876f2d3a12aed18008
VAL <val.shklianik@gmail.com>
2017-11-24 03:29:24 +0300

    fibonacci implementation

---
+++ NET.W.2017.Shklianik.11_12/FibonacciSequence/FibonacciSequenceLogic/FibonacciSequence.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FibonacciSequenceLogic
+{
+    #region public class FibonacciSequence
+    public static class FibonacciSequence
+    {
+        #region static IEnumerable
+        /// <summary>
+        /// generates Fibonacci Sequence
+        /// </summary>
+        /// <returns>next elemebt of sequence</returns>
+        public static IEnumerable<int> GetFibSecuence(int length)
+        {
+            if (length <= 0) throw new ArgumentException(nameof(length));
+            int currentEl = 0;
+            int nextEl = 1;
+
+            while (length > 0)
+            {
+                length--;
+                yield return currentEl;
+                int temp = currentEl + nextEl;
+                currentEl = nextEl;
+
+                nextEl = temp;
+            }
+        }
+        #endregion
+    }
+    #endregion
+}
---
+++ NET.W.2017.Shklianik.11_12/FibonacciSequence/FibonacciSequenceTests/TestFibonacciSeq.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using FibonacciSequenceLogic;
+using System.Linq;
+
+namespace FibonacciSequenceTests
+{
+    [TestFixture]
+    public class TestFibonacciSeq
+    {
+        public static IEnumerable intGenerateFibonacciSequence
+        {
+            get
+            {
+                yield return new TestCaseData(4).Returns(new int[4] { 0, 1, 1, 2 });
+                yield return new TestCaseData(6).Returns(new int[6] { 0, 1, 1, 2, 3, 5 });
+                yield return new TestCaseData(8).Returns(new int[8] { 0, 1, 1, 2, 3, 5, 8, 13 });
+
+            }
+
+        }
+
+        public static IEnumerable ExceptionTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(-1);
+                yield return new TestCaseData(0);
+
+            }
+
+        }
+
+        [Test, TestCaseSource("ExceptionTestCases")]
+        public void ExceptionFibonacciSequenceTest(int length)
+        {
+            Assert.Throws<ArgumentException>(() => FibonacciSequence.GetFibSecuence(length).First());
+        }
+
+        [Test, TestCaseSource("intGenerateFibonacciSequence")]
+        public IEnumerable FibonacciSequenceTest(int length) => FibonacciSequence.GetFibSecuence(length);
+
+        
+    }
+}
