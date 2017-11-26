77227a822f6386e6f6bc29d21595568ba11297e7
VAL <val.shklianik@gmail.com>
2017-11-26 18:45:55 +0300

    queue logic + tasts + styleCop

---
+++ NET.W.2017.Shklianik.13/Queue/QueueLogic.Tests/QueueTest.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace QueueLogic.Tests
+{
+    [TestFixture]
+    public class QueueTest
+    {
+        public static IEnumerable IntEnqueueQueue
+        {
+            get
+            {
+                var qeueInt = new List<int> { 9, 8, 7, 6 };
+                yield return new TestCaseData(qeueInt, 5).Returns(new List<int> { 9, 8, 7, 6, 5 });
+                yield return new TestCaseData(qeueInt, 0).Returns(new List<int> { 9, 8, 7, 6, 0 });
+            }
+        }
+
+        public static IEnumerable IntDequeueQueue
+        {
+            get
+            {
+                var intQueue = new Queue<int>(new List<int> { 9, 8, 7, 6 });
+                yield return new TestCaseData(intQueue).Returns(9);
+                yield return new TestCaseData(intQueue).Returns(8);
+                yield return new TestCaseData(intQueue).Returns(7);
+            }
+        }
+
+        public static IEnumerable IntPeekQueue
+        {
+            get
+            {
+                var qeueInt = new Queue<int>(new List<int> { 9, 8, 7, 6 });
+                yield return new TestCaseData(qeueInt).Returns(9);
+                yield return new TestCaseData(qeueInt).Returns(9);
+                yield return new TestCaseData(qeueInt).Returns(9);
+            }
+        }
+
+        public static IEnumerable IntClearQueue
+        {
+            get
+            {
+                var qeueInt = new List<int> { 9, 8, 7, 6 };
+                yield return new TestCaseData(qeueInt).Returns(true);
+            }
+        }
+
+        public static IEnumerable IntMoveNextQueue
+        {
+            get
+            {
+                var qeueInt = new Queue<int>(new List<int> { 9, 8, 7, 6 });
+                yield return new TestCaseData(qeueInt).Returns(true);
+                yield return new TestCaseData(qeueInt).Returns(true);
+                yield return new TestCaseData(qeueInt).Returns(true);
+                yield return new TestCaseData(qeueInt).Returns(true);
+                yield return new TestCaseData(qeueInt).Returns(false);
+            }
+        }
+        
+        public static IEnumerable IntMovePrevQueue
+        {
+            get
+            {
+                var qeueInt = new Queue<int>(new List<int> { 9, 8, 7, 6 });
+                qeueInt.MoveNext();
+                qeueInt.MoveNext();
+                yield return new TestCaseData(qeueInt).Returns(true);
+                yield return new TestCaseData(qeueInt).Returns(true);
+                yield return new TestCaseData(qeueInt).Returns(false);
+            }
+        }
+
+        [Test, TestCaseSource("IntEnqueueQueue")]
+        public List<int> IntEnqueueQueueTest(List<int> qeue, int element)
+        {
+            var intQueue = new Queue<int>(qeue);
+            var queueReturn = intQueue.Enqueue(element);
+            List<int> list = new List<int>();
+            while (!queueReturn.IsEmpty())
+            {
+                list.Add(queueReturn.Dequeue());
+            }
+
+            return list;
+        }
+
+        [Test, TestCaseSource("IntDequeueQueue")]
+        public int IntDequeueQueueTest(IQueue<int> qeue)
+        {
+            return qeue.Dequeue();
+        }
+
+        [Test, TestCaseSource("IntPeekQueue")]
+        public int IntPeekQueueTest(IQueue<int> qeue)
+        {
+            return qeue.Peek();
+        }
+
+        [Test, TestCaseSource("IntClearQueue")]
+        public bool IntClearQueueTest(List<int> qeue)
+        {
+            var intClear = new Queue<int>(qeue);
+            intClear.Clear();
+            return intClear.IsEmpty();
+        }
+
+        [Test, TestCaseSource("IntMoveNextQueue")]
+        public bool IntMoveNextQueueTest(IIterator<int> qeue)
+        {
+            return qeue.MoveNext();
+        }
+
+        [Test, TestCaseSource("IntMovePrevQueue")]
+        public bool IntMovePrevQueueTest(IIterator<int> qeue)
+        {
+            return qeue.MovePrev();
+        }
+    }
+}
---
+++ NET.W.2017.Shklianik.13/Queue/QueueLogic/Queue.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QueueLogic
+{
+    public interface IQueue<T>
+    {
+        bool IsEmpty();
+
+        IQueue<T> Enqueue(T element);
+
+        T Dequeue();
+
+        T Peek();
+        
+        void Clear();
+    }
+
+    public interface IIterator<T>
+    {
+        bool MoveNext();
+
+        bool MovePrev();
+
+        void Reset();
+
+        T GetCurrent();
+    }
+
+    public class Queue<T> : IQueue<T>, IIterator<T>
+    {
+        private readonly List<T> queue;
+        private int currentIndex;
+
+        public Queue()
+        {
+            queue = new List<T>();
+        }
+
+        public Queue(List<T> queue) : this()
+        {
+            foreach (var q in queue)
+            {
+                this.queue.Add(q);
+            }
+
+            currentIndex = -1;
+        }
+
+        public bool IsEmpty()
+        {
+            return queue.Count == 0;
+        }
+        
+        public IQueue<T> Enqueue(T element)
+        {
+            if (element == null) throw new ArgumentNullException(nameof(element));
+            queue.Add(element);
+            return this;
+        }
+
+        public T Dequeue()
+        {
+            T firstEl = queue[0];
+            queue.Remove(queue[0]);
+            return firstEl;
+        }
+
+        public T Peek()
+        {
+            return queue[0];
+        }
+
+        public void Clear()
+        {
+            queue.Clear();
+        }
+
+        public bool MoveNext()
+        {
+            if (currentIndex < queue.Count)
+            {
+                currentIndex++;
+                return currentIndex < queue.Count;
+            }
+
+            return false;
+        }
+
+        public bool MovePrev()
+        {
+            currentIndex--;
+            return currentIndex >= -1;
+        }
+
+        public void Reset()
+        {
+            currentIndex = -1;
+        }
+
+        public T GetCurrent()
+        {
+            return queue[currentIndex];
+        }
+    }
+}   
