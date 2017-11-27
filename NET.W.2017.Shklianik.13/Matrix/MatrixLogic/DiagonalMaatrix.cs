56f374a477297efa08a07c50039e354438101e36
VAL <val.shklianik@gmail.com>
2017-11-27 03:08:20 +0300

    start impl

---
+++ NET.W.2017.Shklianik.13/Matrix/MatrixLogic/DiagonalMaatrix.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixLogic
+{
+    public class DiagonalMaatrix<T> : SquareMatrix<T>
+    {
+        private T[] diagonalsElements;
+        public DiagonalMaatrix(int order) : base(order)
+        {
+            diagonalsElements = new T[order];
+        }
+
+        public DiagonalMaatrix(T[,] matrix) : base(matrix)
+        {
+        }
+
+        public DiagonalMaatrix(SquareMatrix<T> matrix) : base(matrix)
+        {
+        }
+    }
+}
---
+++ NET.W.2017.Shklianik.13/Matrix/MatrixLogic/SquareMatrix.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixLogic
+{
+    public class SquareMatrix<T> : IEnumerable<T>, IEnumerable
+    {
+        private readonly int order;
+        private T[] matrix;
+
+        public event EventHandler<MatrixEventArgs<T>> OnChangeValue = delegate { };
+
+        public SquareMatrix(int order)
+        {
+            if (order <= 0) throw new ArgumentException($"{nameof(order)} less then 0");
+            this.order = order;
+            this.matrix = new T[order * order];
+        }
+
+        public SquareMatrix(T[,] matrix)
+        {
+            if (ReferenceEquals(matrix, null))
+            {
+                throw new ArgumentNullException(nameof(matrix));
+            }
+
+            if (matrix.GetLength(0) != matrix.GetLength(1))
+            {
+                throw new ArgumentException($"{nameof(matrix)} is not square.");
+            }
+
+            order = matrix.GetLength(0);
+            this.matrix = new T[order * order];
+            for (int i = 0; i <= order; i++)
+            {
+                for (int j = 0; j <= order; j++)
+                {
+                    this.matrix[GetIndex(i, j)] = matrix[i, j];
+                }
+            }
+        }
+
+        public SquareMatrix(SquareMatrix<T> matrix)
+        {
+            if (ReferenceEquals(matrix, null)) throw new ArgumentNullException(nameof(matrix));
+            order = matrix.Order;
+            this.matrix = new T[order * order];
+            for (int i = 0; i <= order; i++)
+            {
+                for (int j = 0; j <= order; j++)
+                {
+                    this[i, j] = matrix[i, j];
+                }
+            }
+        }
+
+        public int Order { get; }
+
+        public T this[int i, int j]
+        {
+            get
+            {
+                VerifyMatrix(i, j);
+                return GetValue(i, j);
+            }
+
+            set
+            {
+                VerifyMatrix(i, j);
+                var oldValue = GetValue(i, j);
+                SetValue(value, i, j);
+                OnChangeValue(this, new MatrixEventArgs<T>(i, j, oldValue, value));
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.Order; i++)
+            {
+                for (int j = 0; j < this.Order; j++)
+                {
+                    yield return this.GetValue(i, j);
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        private void VerifyMatrix(int i, int j)
+        {
+            if (i > order || i <= 0 || j > order || j <= 0) throw new ArgumentException("coords is error");
+        }
+
+        private T GetValue(int i, int j)
+        {
+            return matrix[GetIndex(i, j)];
+        }
+
+        private void SetValue(T value, int i, int j)
+        {
+            matrix[GetIndex(i, j)] = value;
+        }
+
+        private int GetIndex(int i, int j)
+        {
+            return  (i * order) + j;
+        }
+
+    }
+}
